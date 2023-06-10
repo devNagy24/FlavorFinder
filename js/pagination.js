94fe926ffb12bffa1ce0267e01eb89e8f4b2cf44
devNagy <Nagy.Devon@student.greenriver.edu>
2023-06-09 21:42:10 -0400

    added comments throughout the code

--- js/pagination.js
+++ js/pagination.js
@@ -1,6 +1,11 @@
+// Normally I would pass 'e' instead of 'event', but this is more readable
 document.querySelector('.pagination').addEventListener('click', function(event) {
+    // Verifying that the anchor tag is clicked
     if (event.target.tagName === 'A') {
+        // Preventing reloading of page
         event.preventDefault();
+
+        // Looking for href link of "?page=NUMBER"
         let page = parseInt(event.target.href.split('=')[1]);
         loadRecipes(page);
     }
@@ -17,8 +22,10 @@ function loadRecipes(page) {
     })
         .then(response => response.text())
         .then(data => {
-            history.pushState({}, "", `?page=${page}`);  // update URL
-            document.querySelector('.row-cols-1').innerHTML = data; // Here, we assume that the server returns only the recipe cards as HTML
+            // update URL
+            history.pushState({}, "", `?page=${page}`);
+            // Assuming that the server returns only the recipe cards as HTML
+            document.querySelector('.row-cols-1').innerHTML = data;
         })
         .catch((error) => {
             console.error('Error:', error);
